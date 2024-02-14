abe292c29ef72014490b7c273ae5194b95ce11f9
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2024-02-14 05:18:59 +0500

    Add associations in Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  # Associations
+  belongs_to :users, foreign_key: 'user_id', class_name: 'User'
+  belongs_to :posts, foreign_key: 'post_id', class_name: 'Post'
 end
