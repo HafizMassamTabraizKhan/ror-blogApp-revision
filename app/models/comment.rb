d37e33bd36271a478c675084379b5f47f86dc6f9
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2024-02-14 05:55:05 +0500

    Add methods in Comment model to increment and decrement comments counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,17 @@ class Comment < ApplicationRecord
   # Associations
   belongs_to :users, foreign_key: 'user_id', class_name: 'User'
   belongs_to :posts, foreign_key: 'post_id', class_name: 'Post'
+
+  # Callback
+  after_save :increment_comments_counter_for_post
+  after_destroy :decrement_comments_counter_for_post
+
+  # Methods
+  def increment_comments_counter_for_post
+    post.increment!(:comments_counter)
+  end
+
+  def decrement_comments_counter_for_post
+    post.decrement!(:comments_counter)
+  end
 end
