cc7097ed747a1f96db04b34b5213c420292feaa1
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2024-02-14 05:51:29 +0500

    Add method in User model to get 3 most recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', class_name: 'Post', dependent: :destroy
   has_many :comments, foreign_key: 'user_id', class_name: 'Comment', dependent: :destroy
   has_many :likes, foreign_key: 'user_id', class_name: 'Like', dependent: :destroy
+
+  # Attributes
+  attribute :name, :string
+  attribute :photo, :string
+  attribute :bio, :text
+  attribute :posts_counter, :integer, default: 0
+
+  # Methods
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
