1f885078d4461d27aa6634a4cb78f20da02b89b1
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2024-02-14 05:10:19 +0500

    Add association in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  # Associations
+  has_many :posts, foreign_key: 'author_id', class_name: 'Post', dependent: :destroy
+  has_many :comments, foreign_key: 'user_id', class_name: 'Comment', dependent: :destroy
+  has_many :likes, foreign_key: 'user_id', class_name: 'Like', dependent: :destroy
 end
